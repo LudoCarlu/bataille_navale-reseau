e92660c78676e59aa31b3cbb60e9f8facf0897f2
LudoCarlu <ludoviccarlu@gmail.com>
2018-09-22 19:10:34 +0200

    refonte serveur pour tour à tour

--- battleship_server.py
+++ battleship_server.py
@@ -7,80 +7,75 @@ Created on Tue Sep 11 11:38:43 2018
 """
 
 import socket
-import threading
 import select
 import time
+import sys
+import queue
 
 hote = ''
 port = 2018
         ## message_description[messageclient] ->
         # message_description[messageclient][0] -> Réponse du serveur
         # message_description[messageclient][1] -> Fonction a appeler
-message_description_server = { 
+code_transcription = {
+        "login_admin" : ["Authentification successfull", "Authentification failed"],
+        "login_joueur" : ["Authentification successfull", "Authentification failed"],
         'logina': ('Mot de passe :','login()'),
         'loginj': ('Mot de passe','login()'),
         'supprimera': ('Quel client à supprimer ?','supprimerclient'),
         'jouerj': ('?','lancer_tir'),
         }
 
-class ClientThread(threading.Thread):
-
-    def __init__(self, ip, port, clientsocket):
-
-        threading.Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.clientsocket = clientsocket
-        print("[+] Nouveau thread pour %s %s" % (self.ip, self.port, ))
-
-
-    def get_clientsocket(self):
-        return self.clientsocket
-    """
-    def set_x(self, x):
-        self.__x = x
-    """
-    
-    def run(self): 
-   
-        print("Connexion de %s %s" % (self.ip, self.port, ))
-
-        #r = self.clientsocket.recv(2048)
-        #print("Ouverture du fichier: ", r, "...")
-        #fp = open(r, 'rb')
-        #self.clientsocket.send(fp.read())
-
-        #print("Client déconnecté...")
-        #self.clientsocket.close()
         
-
-##########
-        
-serveur_actif = True
-clients_connectes = []
-
 connexion_du_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_du_serveur.setblocking(0)
 connexion_du_serveur.bind((hote, port))
 print("socket is binded to %s" %(port))
 connexion_du_serveur.listen(3) #Serveur écoute jusqu'à 3 connexions
 print( "socket is listening")
 
+#inputs = liste de toutes les connexions
+inputs = [connexion_du_serveur]
+outputs = []
+queue_des_messages = {}
 
-while serveur_actif:
+serveur_actif = True
+
+while inputs:
     # On va vérifier que de nouveaux clients ne demandent pas à se connecter
-    # Pour cela, on écoute la connexion_principale en lecture
+    # Pour cela, on écoute la connexion du serveur en lecture
     # On attend maximum 50ms
 
-    connexions_demandees, wlist, xlist = select.select([connexion_du_serveur], [], [], 0.05)
+    readable, writable, exceptional = select.select(inputs, outputs, inputs, 0.05)
     
-    for connexion in connexions_demandees:
-        connexion_avec_client, infos_connexion = connexion.accept()
-        print("Got connexion from %s : %s" % (infos_connexion[0], infos_connexion[1]))
-        # On ajoute le thread contenant le socket connecté à la liste des clients
-        newthread = ClientThread(infos_connexion[0], infos_connexion[1], connexion_avec_client)
-        newthread.start()
-        clients_connectes.append(newthread.get_clientsocket())
-    
+    for connexion in readable:
+        
+        if connexion is connexion_du_serveur: #s est le serveur
+            
+            connexion_avec_client, adresse_du_client = connexion.accept()
+            connexion.setblocking(0)
+            print("Connexion de %s : %s" % (adresse_du_client[0], adresse_du_client[1]))
+            
+            inputs.append(connexion_avec_client)
+            #On crée une liste pour les messages de ce client
+            queue_des_messages[connexion_avec_client] = queue.Queue()
+            
+        else:
+            message_du_client = connexion.recv(1024)
+            print(adresse_du_client[0],":",message_du_client.decode())
+            
+            if message_du_client:
+                queue_des_messages[connexion].put(message_du_client)
+                if connexion not in outputs:
+                    outputs.append(connexion)
+            
+            else:
+                if connexion in outputs:
+                    outputs.remove(connexion)
+                inputs.remove(connexion)
+                connexion.close()
+                del queue_des_messages[connexion]
+            
     # Maintenant, on écoute la liste des clients connectés
     # Les clients renvoyés par select sont ceux devant être lus (recv)
     # On attend là encore 50ms maximum
@@ -88,35 +83,26 @@ while serveur_actif:
     # En effet, si la liste de clients connectés est vide, une exception
     # Peut être levée
     
-    clients_a_lire = []
-    try:
-        """
-        rlist: wait until ready for readingvc
-        wlist: wait until ready for writing
-        xlist: wait for an “exceptional condition”
-        """
-        #client a lire : rlist
-        clients_a_lire, wlist, xlist = select.select(clients_connectes, [], [], 0.05)
-    except select.error:
-        pass
-    else:
-        # On parcourt la liste des clients à lire
-        for client in clients_a_lire:
-            # Client est de type socket
-            msg_recu = client.recv(1024)
-            # Peut planter si le message contient des caractères spéciaux
-            msg_recu = msg_recu.decode()
-            print("Reçu {}".format(msg_recu))
-            client.send(b"5 / 5")
+    for connexion in writable:
+    
+        try:
+            message_suivant = queue_des_messages[connexion].get_nowait()
             
-            if msg_recu == "fin":
-                print("Client s'est déconnecté")
-                client.close()
-                
-                
-"""
-print("Fermeture des connexions")
-for client in clients_connectes:
-    client.close()
-"""
-connexion_principale.close()
+        except queue.Empty:
+            outputs.remove(connexion)
+            
+        else:
+            connexion.send(message_suivant)
+    
+    
+    for connexion in exceptional:
+        
+        inputs.remove(connexion)
+        
+        if connexion in outputs:
+            outputs.remove(s)
+        
+        connexion.close()
+        del queue_des_messages[connexion]
+        
+
--- client.py
+++ client.py
@@ -1,14 +1,16 @@
 import socket
 from Game import *
 
-hote = "localhost"
+hote = 'localhost'
 port = 2018
 
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_avec_serveur.connect((hote, port))
 print("Connexion établie avec le serveur sur le port {}".format(port))
 
+
 msg_a_envoyer = b""
+"""
 while msg_a_envoyer != b"fin":
     print('Login :')
     msg_a_envoyer = input("> ")
@@ -31,6 +33,40 @@ while msg_a_envoyer != b"fin":
     connexion_avec_serveur.send(msg_a_envoyer)
     msg_recu = connexion_avec_serveur.recv(1024)
     print(msg_recu.decode()) # Là encore, peut planter s'il y a des accents
+"""
 
+"""
+Dans les envoi de donner {code, data}
+
+login_admin : (login, mdp)
+login_joueur : (login, mdp)
+
+"""
+a_envoyer = ""
+data = ""
+code = ""
+
+print("Authentifiez vous :")
+print("Login :", end =" ")
+login = input()
+print("Password :", end=" ")
+mdp = input()
+
+if login  == 'admin':
+    code = "login_admin"
+    data = (login,mdp)
+    
+else:
+    code = "login_joueur"
+    data = (login, mdp)
+
+a_envoyer = code + ';' + str(data)
+#Envoyer code 
+connexion_avec_serveur.send(a_envoyer.encode())
+
+reponse_login = connexion_avec_serveur.recv(1024)
+print(repr(reponse_login))
+    
+    
 print("Fermeture de la connexion")
-#connexion_avec_serveur.close()
+connexion_avec_serveur.close()
